a6867d7f1c2ae895bdb9f66f4d3c6bfeaa0a88c9
beeleza <higorbeleza.dev@gmail.com>
2026-05-11 23:05:18 -0300

    recuperando porta da instancia em execução

--- exchange-service/src/main/java/com/beeleza/exchange_service/controller/ExchangeController.java
+++ exchange-service/src/main/java/com/beeleza/exchange_service/controller/ExchangeController.java
@@ -1,6 +1,8 @@
 package com.beeleza.exchange_service.controller;
 
+import com.beeleza.exchange_service.environment.InstanceInformationService;
 import com.beeleza.exchange_service.model.Exchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +15,15 @@ import java.math.BigDecimal;
 @RequestMapping("exchange-service")
 public class ExchangeController {
 
+    @Autowired
+    private InstanceInformationService informationService;
+
     // http://localhost:8000/exchange-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(
             @PathVariable BigDecimal amount,
             @PathVariable String from,
             @PathVariable String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
     }
 }
