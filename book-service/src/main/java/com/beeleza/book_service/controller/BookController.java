bbd3cf6c86f4698a58069d456657c4700ce73199
beeleza <higorbeleza.dev@gmail.com>
2026-05-18 09:10:34 -0300

    consumindo informações deexchange service no book service

--- book-service/src/main/java/com/beeleza/book_service/controller/BookController.java
+++ book-service/src/main/java/com/beeleza/book_service/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.beeleza.book_service.controller;
 
+import com.beeleza.book_service.dto.Exchange;
 import com.beeleza.book_service.environment.InstanceInformationService;
 import com.beeleza.book_service.model.Book;
 import com.beeleza.book_service.repository.BookRepository;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -32,7 +35,18 @@ public class BookController {
 
         var book = bookRepository.findById(id).orElseThrow();
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/exchange-service/" + "{amount}/{from}/{to}", Exchange.class, params);
+
+        Exchange exchange = response.getBody();
+
         book.setEnvironment(port);
+        book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
 
         return book;
---
+++ book-service/src/main/java/com/beeleza/book_service/dto/Exchange.java
@@ -0,0 +1,82 @@
+package com.beeleza.book_service.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Exchange implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String from;
+
+    private String to;
+
+    private BigDecimal conversionFactor;
+
+    private Double convertedValue;
+
+    private String environment;
+
+    public Exchange() {
+    }
+
+    public Exchange(Long id, String from, String to, BigDecimal conversionFactor, Double convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public Double getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(Double convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
+
